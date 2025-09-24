fcce733c88570b322e8421952a447796e6476910
Tim Stilwell <inquisitivefrog@gmail.com>
2025-09-24 14:04:14 -0700

    experiments with VS Code debugging

--- w3schools/debug/will_crash/nil_pointer_referenced.go
+++ w3schools/debug/will_crash/nil_pointer_referenced.go
@@ -3,13 +3,16 @@ package main
 import "fmt"
 
 type Person struct {
-    Name string
+	Name string
 }
 
 func main() {
-    var p *Person // Declare a pointer to Person, but it's nil
-    fmt.Println("Program started")
-    // This will cause a runtime panic: nil pointer dereference
-    fmt.Println(p.Name)
-    fmt.Println("This line will not be reached")
+	var t *Person
+	var p *Person // Declare a pointer to Person, but it's nil
+	fmt.Println("Program started")
+	t = &Person{Name: "Bruce Wayne"}
+	fmt.Println(t.Name)
+	// This will cause a runtime panic: nil pointer dereference
+	fmt.Println(p.Name)
+	fmt.Println("This line will not be reached")
 }
